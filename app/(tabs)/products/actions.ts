dc431f261b07e31bec97cccb67d85fd265bcfbe2
SM <52240990+baesumin@users.noreply.github.com>
2024-05-22 14:57:13 +0900

    11-0

---
+++ app/(tabs)/products/actions.ts
@@ -0,0 +1,20 @@
+"use server";
+import db from "@/lib/db";
+
+export async function getMoreProducts(page: number) {
+  const products = await db.product.findMany({
+    select: {
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+      id: true,
+    },
+    skip: page * 1,
+    take: 1,
+    orderBy: {
+      created_at: "desc",
+    },
+  });
+  return products;
+}
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -48,7 +48,12 @@ export default async function ProductDetail({
   return (
     <div>
       <div className="relative aspect-square">
-        <Image fill src={product.photo} alt={product.title} />
+        <Image
+          fill
+          className="object-cover"
+          src={product.photo}
+          alt={product.title}
+        />
       </div>
       <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
         <div className="size-10 rounded-full overflow-hidden">
